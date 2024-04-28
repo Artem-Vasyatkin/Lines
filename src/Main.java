b2e0680c442b284cfaff733306cc1b9f2d628dd1
Artem Vasyatkin <dustler98@gmail.com>
2024-04-28 14:41:03 +0300

    Training done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phone = "+(918)-957 68 28";
+        phone = '7' + phone;
+        phone = phone.replace(" ", "");
+        phone = phone.replace(")", "");
+        phone = phone.replace("(", "");
+        phone = phone.replace("+", "");
+        phone = phone.replace("-", "");
+
+        if (phone.length() == 10) {
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() <= 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас посторонний!");
+        }
+        System.out.println("phone = " + phone);
+
+        String expectedPhone = "79189576828";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
     }
 }
